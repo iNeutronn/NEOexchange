fda034e84dea004986e10c63e4c8147f41bf0c1f
Neutron <neutron26332@gmail.com>
2020-09-29 13:20:52 +0300

    Додано можливість  зчитувати дані аваль банку

--- NEOexchange/AvalBankReader.cs
+++ NEOexchange/AvalBankReader.cs
@@ -12,11 +12,29 @@ namespace NEUexchange
             resp = wc.DownloadString("https://ex.aval.ua/personal/everyday/exchange/exchange/");
 
             id = IndexOf(resp, "<td class=\"name\">Долари&nbsp;США</td>");
-            
-            
+
+
+            ToBuyAdd(BaseСurrency.USD, NextDigit());
+            ToSellAdd(BaseСurrency.USD, NextDigit());
+
+            ToBuyAdd(BaseСurrency.EUR, NextDigit());
+            ToSellAdd(BaseСurrency.EUR, NextDigit());
+
+            ToBuyAdd(BaseСurrency.RUB, NextDigit());
+            ToSellAdd(BaseСurrency.RUB, NextDigit());
+
+        }
+        decimal NextDigit()
+        {
+            while (!char.IsDigit(resp[id])) id++;
+
+            string course = "";
+            for (int i = id; resp[i] == ',' | char.IsDigit(resp[i]); i++)
+                course += resp[i];
+            return decimal.Parse(course);
         }
 
-        public static int IndexOf(string s, string f, int start = 0, int stop = 0)
+        int IndexOf(string s, string f, int start = 0, int stop = 0)
         {
             if (stop == 0)
                 stop = s.Length;
